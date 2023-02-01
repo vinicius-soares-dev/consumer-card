d54bf93a26ae87e563715c27fb8d480a2c2f3068
Vinicius Soares <106778671+vinicius-soares-dev@users.noreply.github.com>
2023-02-01 12:49:45 -0300

    frontend integration

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './globalStyle.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Olá eestou aqui</h1>
+    </div>
+  );
+}
+
+export default App;
